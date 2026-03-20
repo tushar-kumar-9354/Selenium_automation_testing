e7e85d37bb7a1495b3bf1601fea0b5472218b7f0
kpregtech <kpregtech@gmail.com>
2026-03-20 20:33:10 +0530

    added github actions

--- TestPilotPro/utils/driver_factory.py
+++ TestPilotPro/utils/driver_factory.py
@@ -1,25 +1,35 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
 from config import BROWSER
 
 def get_driver():
 
-    options = webdriver.ChromeOptions()
-
-    options.add_experimental_option("prefs", {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False
-    })
-
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--start-maximized")
-
     if BROWSER == "chrome":
+
+        options = webdriver.ChromeOptions()
+
+        options.add_experimental_option("prefs", {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False
+        })
+
+        options.add_argument("--disable-notifications")
+
         driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()),
+            service=ChromeService(ChromeDriverManager().install()),
             options=options
         )
 
+    elif BROWSER == "firefox":
+
+        driver = webdriver.Firefox(
+            service=FirefoxService(GeckoDriverManager().install())
+        )
+
+    driver.maximize_window()
     return driver
